fe0b0b0a00e233fa8d29b4ab052e8f242cc23345
Nicole <1148620180@qq.com>
2022-08-29 15:49:53 +0800

    fix:文件路径创建失败时，panic 并打印日志原因

--- cmd/fileUrl.go
+++ cmd/fileUrl.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"gitee.com/nicole-go-libs/print-colors/color_print"
 	"os"
 	"sqltostruct/config"
 )
 
 func InitFileUrl() {
 	if err := os.MkdirAll(config.Conf.SaveFilePwd, 0755); err != nil {
-		return
+		fmt.Println(color_print.Red(fmt.Sprintf("初始化文件路径创建失败，失败原因：\n%s", err)))
+		panic(err)
 	}
 }
